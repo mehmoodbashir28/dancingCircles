fac09e55f6163dd4eca91e4a07bbba547ac3571d
Mehmood Bashir <39158675+mehmoodbashir28@users.noreply.github.com>
2018-09-22 17:35:59 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-function initialize(){
-	let circleContainer = document.querySelector(".circleContainer");
-	for(let i=0; i<12; i++){
-		circleContainer.innerHTML += `<div id="circle" class="div-${i}"></div>`;
-		changeCircle(i);
-		}
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
-function changeCircle(index){
-		const top = Math.random() * 100;
-		const left = Math.random() * 100;
-		const divX = document.querySelector(`.div-${index}`);
-		divX.style.top=`${top}%`;
-		divX.style.left=`${left}%`;
-		const height= Math.random() * 400;
-		const width= Math.random() * 400;
-		divX.style.height=`${height}px`;
-		divX.style.width=`${height}px`;
-		divX.innerHTML=Math.floor(height);
-
-
-
-		
+function initialize() {
+	let circleC = document.querySelector("#circle-container");
+	for (let i = 0; i < 12; i++){
+		circleC.innerHTML += `<div id="div-${i}"
+		class="circle"><div>`;
+		setInterval(function(){
+			changeCircle(i);
+		},1000);
 	}
+	
+}
+
+function changeCircle(index){
+		let top	= parseInt(Math.random() * 100);
+		let right = parseInt(Math.random() * 100);
+		let divX = document.querySelector(`#div-${index}`);
+		divX.style.top = `${top}%`;
+		divX.style.right = `${right}%`;
+		let height = getRandomIntInclusive(30,200);
+		divX.style.height = `${height}px`;
+		divX.style.width = `${height}px`;
+		divX.innerHTML = height;
+		divX.style["font-size"] = `${height/3}px`;
+		let red = parseInt(Math.random() * 256);
+		let green = parseInt(Math.random() * 256);
+		let blue = parseInt(Math.random() * 256);
+		let color = `rgb(${red},${green},${blue})`;
+		divX.style.color = color;
+		divX.style["border-color"] = color;
+		divX.style["border-width"] = `${height/30}px`
+}
